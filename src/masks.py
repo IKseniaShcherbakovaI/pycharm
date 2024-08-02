d40785ba1d4aa8ff7c0fa05a488c2e5e07106644
ксения щербакова <ksenia75423@mail.ru>
2024-08-02 20:10:35 +1000

    add src

---
+++ src/masks.py
@@ -0,0 +1,9 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскирующая номер карты"""
+    return f"{card_number[:4]} {card_number[4:6]} {'*' * 2} {'*' * 4} {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция маскирующая номер счета"""
+    return f"{'*' * 2}{account_number[-4:]}"
+
