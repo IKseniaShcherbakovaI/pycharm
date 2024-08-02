52db54e465e9c4d126374989adac319b8a27e04f
ксения щербакова <ksenia75423@mail.ru>
2024-08-02 22:16:22 +1000

    add src\widget.py fix

--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,25 @@
+from masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account_card: str) -> str:
-    """Функция маскирующая как номер карты так и номер счета"""
+    """Функция возвращающая как маску карты так и маску счета"""
     result = account_card.split()
     result_mask = ""
     if result[0] == "Счет":
         for items in result:
             if items.isdigit():
-                result_mask += f"{result[0]} {'*' * 2}{items[-4:]}"
+                result_mask += f"{result[0]} {get_mask_account(items)}"
     else:
         for items in result:
             if items.isalpha():
                 result_mask += items + " "
             elif items.isdigit():
-                result_mask += f"{items[:4]} {items[4:6]} {'*' * 2} {'*' * 4} {items[12:]}"
+                result_mask += f"{items[:4]} {get_mask_card_number(items)}"
     return result_mask
 
 
 def get_date(date: str) -> str:
-    """Функция возвращающая даты"""
+    """Функция возвращающая дату"""
     result = date.replace("-", "")
     for date in result:
         return f"{result[6:8]}.{result[4:6]}.{result[0:4]}"
