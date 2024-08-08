3ff8cdefdab1a51e05a46466853c1eaad6d83852
ксения щербакова <ksenia75423@mail.ru>
2024-08-09 01:08:08 +1000

    add tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,11 @@
-from masks import get_mask_account, get_mask_card_number
-
-
-def test_get_mask_account():
-    assert get_mask_account('7000792289606361') == '7000 79** **** 6361'
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
-    assert get_mask_card_number('73654108430135874305 ') == '**4305'
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
+    assert get_mask_card_number("8990922113665229") == "8990 92** **** 5229"
+
+
+def test_get_mask_account():
+    assert get_mask_account("73654108430135874305") == "**4305"
