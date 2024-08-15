700a07ecbe72b90834ba873bee7f684035b7aa49
ксения щербакова <ksenia75423@mail.ru>
2024-08-15 13:24:51 +1000

    add generators.py fix

--- src/generators.py
+++ src/generators.py
@@ -1,68 +1,34 @@
-def filter_by_currency(transactions: list, state: str) -> list:
-    '''Функция выдающая список тразакций по определенной валюте'''
+from typing import Generator
+
+
+def filter_by_currency(transactions: list, state: str) -> Generator:
+    """Функция выдающая список тразакций по определенной валюте"""
     if transactions == []:
-        exit('Нет транзакций')
+        exit("Нет транзакций")
 
     for i in transactions:
-        if i.get("operationAmount").get('currency').get('code') == state:
+        if i.get("operationAmount").get("currency").get("code") == state:
             yield i
 
     for i in transactions:
-        if state not in i.get("operationAmount").get('currency').get('code') or state == '':
-            exit('С данной валютой не было транзакций')
+        if state not in i.get("operationAmount").get("currency").get("code") or state == "":
+            exit("С данной валютой не было транзакций")
 
 
-def transaction_descriptions(transactions):
-    '''Функция возвращающая описание по кажой операции'''
+def transaction_descriptions(transactions: list) -> Generator:
+    """Функция возвращающая описание по кажой операции"""
     if transactions == []:
-        exit('Нет транзакций')
+        exit("Нет транзакций")
 
     for i in transactions:
         if i.get("description"):
             yield i.get("description")
 
 
-
-def card_number_generator(start= 1, stop= 99):
-    '''Функция генерирующая номер карт в заданном диапазоне'''
+def card_number_generator(start=1, stop=99) -> Generator:
+    """Функция генерирующая номер карт в заданном диапазоне"""
     for _ in range(start, stop + 1):
         card_number = str(_)
         while len(card_number) < 16:
-            card_number = '0' + card_number
-        yield f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            card_number = "0" + card_number
+        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
