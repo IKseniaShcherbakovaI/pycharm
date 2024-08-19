42275d5e522aff55a5b6f8f782faf2205d316f1c
ксения щербакова <ksenia75423@mail.ru>
2024-08-19 11:26:26 +1000

    add test_decorators.py fix

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -7,14 +7,17 @@ def test_log1():
     @log(filename="test_log.txt")
     def my_function(x, y):
         return x + y
+
     result = my_function(2, 2)
 
     assert result == 4
+
 
 def test_log2(capsys):
     @log(filename="test_log.txt")
     def my_function(x, y):
         return x + y
+
     # Проверка ошибки
     try:
         my_function()
