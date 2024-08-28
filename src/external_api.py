2ed41b65ba50fb446e3c95669567e1f2d1fc6c57
ксения щербакова <ksenia75423@mail.ru>
2024-08-28 23:47:27 +1000

    add test_external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -23,35 +23,3 @@ def transaction_amount(transaction):
         return result["result"]
     else:
         return float(amount)
-
-
-# print(
-#     transaction_amount(
-#         transaction={
-#             "id": 441945886,
-#             "state": "EXECUTED",
-#             "date": "2019-08-26T10:50:58.294041",
-#             "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "USD"}},
-#             "description": "Перевод организации",
-#             "from": "Maestro 1596837868705199",
-#             "to": "Счет 64686473678894779589",
-#         }
-#     )
-# )
-#
-# print(type(
-#     transaction_amount({
-#         "id": 441945886,
-#         "state": "EXECUTED",
-#         "date": "2019-08-26T10:50:58.294041",
-#         "operationAmount": {
-#             "amount": "31957.58",
-#             "currency": {
-#                 "name": "руб.",
-#                 "code": "RUB"}
-#         },
-#         "description": "Перевод организации",
-#         "from": "Maestro 1596837868705199",
-#         "to": "Счет 64686473678894779589"
-#     })
-# ))
