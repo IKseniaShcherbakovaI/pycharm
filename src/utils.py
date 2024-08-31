493222b631b1a62af112e15d44a4edc4ef464ece
ксения щербакова <ksenia75423@mail.ru>
2024-08-31 15:30:27 +1000

    add utils.py new

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,23 @@
 import json
+import logging
+import os
 from json import JSONDecodeError
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def transaction(path):
     """функция возвращающая список словарей с данными о финансовых транзакциях"""
     try:
+        logger.info(f"Ищем файл по указанному пути {path}")
         with open(path, encoding="utf-8") as file:
             data = json.load(file)
     except FileNotFoundError:
