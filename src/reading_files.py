7da5da6ad605eec636daeecc0a341e034eaf21c9
ксения щербакова <ksenia75423@mail.ru>
2024-09-04 09:19:34 +1000

    add reading_files.py

---
+++ src/reading_files.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def transaction_csv(path_csv):
+    ''' Функция принимающая на вход путь до csv файла и возвращающая список словарей с данными о финансовых транзакциях'''
+    if not path_csv:
+        return []
+    try:
+        df = pd.read_csv(path_csv, delimiter=';')
+        return list(df.to_dict(orient="records"))
+    except Exception:
+        return []
+
+
+def transaction_excel(path_excel):
+    ''' Функция принимающая на вход путь до excel файла и возвращающая список словарей с данными о финансовых транзакциях'''
+    if not path_excel:
+        return []
+    try:
+        df = pd.read_excel(path_excel)
+        return list(df.to_dict(orient="records"))
+    except Exception:
+        return []
+
+# print(transaction_csv('C:/Users/Ксения/Desktop/homework/data/transactions.csv'))
+# print(transaction_csv(''))
+# print(transaction_excel(''))
+# print(transaction_excel('C:/Users/Ксения/Desktop/homework/data/transactions_excel.xlsx'))
