1b717b656c5d54ef8baab535ce7b62e04b5740aa
ксения щербакова <ksenia75423@mail.ru>
2024-09-05 23:16:49 +1000

    add test

---
+++ tests/test_search.py
@@ -0,0 +1,7 @@
+# [{'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
+# ['Перевод со счета на счет 1', 'Перевод организации 3']
+# это результаты работы функций
+# print(search_string(transaction, "Перевод со счета на счет"))
+# print(category_search(transaction, ["Перевод со счета на счет", "Перевод организации"]))
+# print(category_search(transaction))
+# принты функций
